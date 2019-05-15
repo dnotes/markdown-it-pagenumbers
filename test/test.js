ca37eca4e119f86f83c52b16b8ad45ff8d97e5a0
David Hunt <git@dnotes.net>
2019-05-15 15:55:10 -0700

    fixed test headers

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ var generate = require('markdown-it-testgen')
 
 /*eslint-env mocha*/
 
-describe('markdown-it-pagenumbers', function () {
+describe('Tests for page number markdown-it plugin', function () {
   var md = require('markdown-it')().use(require('../'))
-  generate(path.join(__dirname, 'fixtures/definitions.txt'), md)
+  generate(path.join(__dirname, 'fixtures/definitions.txt'), { header: true }, md)
 })
